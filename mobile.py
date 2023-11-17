1d9efa950d8ce792fbdaba32c2a2f8352a4ef250
Hafssa haddad <146876902+Hafssa90@users.noreply.github.com>
2023-11-17 02:06:10 +0100

    Add files via upload

---
+++ mobile.py
@@ -0,0 +1,14 @@
+class mobile :
+    def __init__ (self,name,company_name,storage,serial_num,dual_sim,support4k) :
+        self.name=Name
+        self.company_name=company_name
+        self.serial_num=serial_num
+        self.storage.storage
+        self.dual_sim=dual_sim
+        self.support4k=support4k
+    def call(self):
+        print(self.name , "call")
+    def send_message(self):
+        print(self.name , "send_message")
+    def play_media(self):
+        print(self.name , "play_media")
