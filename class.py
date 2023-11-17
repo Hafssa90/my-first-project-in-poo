91e78c904cef5b083341a6fedcd98d14663cf265
Hafssa haddad <146876902+Hafssa90@users.noreply.github.com>
2023-11-17 02:01:48 +0100

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class player :
+    def __init__ (self,name,age,rank) :
+        self.name=Name
+        self.age=age
+        self.rank=rank
+    def pass(self):
+        print(self.name , "pass")
+    def shoot(self):
+        print(self.name , "shoot")
+    def jump(self):
+        print(self.name , "jump")
